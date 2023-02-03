0f7147e4c438eb922795b7f8d69e2cce9d909f47
Phearin Yeun <phearin.yeun@sbilhbank.com.kh>
2023-02-03 10:06:14 +0700

    uploda

---
+++ src/main/java/dev/danvega/springBootJpa/model/User.java
@@ -0,0 +1,10 @@
+package dev.danvega.springBootJpa.model;
+
+import java.time.ZonedDateTime;
+
+public class User {
+    private String name;
+    private Integer age;
+    private ZonedDateTime birthDate;
+    private Boolean active;
+}
