3076b97294cc1576b95ee963acfc03bfb252d551
Phearin Yeun <phearin.yeun@sbilhbank.com.kh>
2023-02-21 17:04:00 +0700

    upload

--- src/main/java/dev/danvega/springBootJpa/Application.java
+++ src/main/java/dev/danvega/springBootJpa/Application.java
@@ -1,18 +1,35 @@
 package dev.danvega.springBootJpa;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+//	@Rule
+//	public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("target/generated-snippets");
+//
+//	@Autowired
+//	private WebApplicationContext context;
+//	private MockMvc mockMvc;
+//
+//	@Before("docs")
+//	public void setUp(){
+//		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+//				.apply(documentationConfiguration(this.restDocumentation))
+//				.build();
+//	}
+//
+//
+//	public void setUp(WebApplicationContext webApplicationContext,
+//					  RestDocumentationContextProvider restDocumentation) {
+//		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+//				.apply(documentationConfiguration(restDocumentation)).build();
+//	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
 	}
 
-	CommandLineRunner commandLineRunner(){
-		return args ->
-				System.out.printf("Testing");
-	}
 }
--- src/main/java/dev/danvega/springBootJpa/model/User.java
+++ src/main/java/dev/danvega/springBootJpa/model/User.java
@@ -5,7 +5,6 @@ import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
 
 @Data
 @Getter
